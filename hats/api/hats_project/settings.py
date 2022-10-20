38bfd863ba1c7203f3f8ec3e894467c37f8708be
Angel Sierra <asierra2002@yahoo.com>
2022-10-20 11:50:48 -0400

    push m8

--- hats/api/hats_project/settings.py
+++ hats/api/hats_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "hats_rest",
     "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class LocationVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
+
+class Hat(models.Model):
+    model = models.CharField(max_length=200)
+    fabric = models.CharField(max_length=200)
+    style_name = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    picture_url = models.URLField(null=True)
+    Location = models.ForeignKey(
+        LocationVO,
+        related_name="Hats",
+        on_delete=models.CASCADE,
+    )
